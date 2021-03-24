6b77b1b30774369248f2eb6948e19aee79cbaaa7
kusnadi99 <muh.kusnadi99@gmail.com>
2021-03-24 23:01:08 +0700

    add middleware check for AuthPageController

--- app/Http/Controllers/AuthPageController.php
+++ app/Http/Controllers/AuthPageController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class AuthPageController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest')->except(['logout', 'auth.index']);
+    }
+
     public function index()
     {
         return view('pages.front.auth', ['headerColor' => true]);
