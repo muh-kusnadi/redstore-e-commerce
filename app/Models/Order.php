dc9f6d34aaee4c303d5efb0b6838b50dd0ddd97b
kusnadi99 <muh.kusnadi99@gmail.com>
2021-03-28 22:01:24 +0700

    add orders model

---
+++ app/Models/Order.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $table = 'orders';
+
+    protected $fillable = ['product_id', 'user_id', 'quantity', 'total', 'is_checkout'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
