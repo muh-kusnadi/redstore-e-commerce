e8bd8755026fec28de80e3c3c1994de449213e21
kusnadi99 <muh.kusnadi99@gmail.com>
2021-03-29 07:44:00 +0700

    retrieve products from database

--- resources/views/pages/front/products.blade.php
+++ resources/views/pages/front/products.blade.php
@@ -14,160 +14,20 @@
     </div>
    <!-- start all products products -->
    <div class="row">
-     <div class="col-4">
-         <a href="{{ route('product.detail', [1]) }}"><img src="{{ asset('assets/images/product-1.jpg') }}" alt="product-1"></a>
-         <a href="{{ route('product.detail', [1]) }}"><h4>Red Printed T-shirt</h4></a>
-         <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-o"></i>
+      @foreach ($products as $item)
+         <div class="col-4">
+            <a href="{{ route('product.detail', $item->id) }}"><img src="{{ asset('storage/assets/images/gallery/1080x1440').'/'.$item->imageUploaded[0]->name }}" alt="{{ $item->slug }}"></a>
+            <a href="{{ route('product.detail', $item->id) }}"><h4>{{ $item->title }}</h4></a>
+            <div class="rating">
+               @for ($i = 0; $i < 5; ++$i)
+                  <i class="fa fa-star{{ $item->rating <= $i ? '-o' : '' }}"></i>
+               @endfor
+            </div>
+            <p>${{ $item->price }}</p>
          </div>
-         <p>$50.00</p>
-      </div>
-      <div class="col-4">
-         <img src="{{ asset('assets/images/product-2.jpg') }}" alt="product-2">
-         <h4>Red Printed T-shirt</h4>
-         <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-o"></i>
-         </div>
-         <p>$50.00</p>
-      </div>
-      <div class="col-4">
-         <img src="{{ asset('assets/images/product-3.jpg') }}" alt="product-3">
-         <h4>Red Printed T-shirt</h4>
-         <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-half-o"></i>
-         </div>
-         <p>$50.00</p>
-      </div>
-      <div class="col-4">
-         <img src="{{ asset('assets/images/product-4.jpg') }}" alt="product-4">
-         <h4>Red Printed T-shirt</h4>
-         <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-o"></i>
-         </div>
-         <p>$50.00</p>
-      </div>
-      <div class="col-4">
-         <img src="{{ asset('assets/images/product-5.jpg') }}" alt="product-5">
-         <h4>Red Printed T-shirt</h4>
-         <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-o"></i>
-         </div>
-         <p>$50.00</p>
-      </div>
-      <div class="col-4">
-         <img src="{{ asset('assets/images/product-6.jpg') }}" alt="product-6">
-         <h4>Red Printed T-shirt</h4>
-         <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-half-o"></i>
-         </div>
-         <p>$50.00</p>
-      </div>
-      <div class="col-4">
-         <img src="{{ asset('assets/images/product-7.jpg') }}" alt="product-7">
-         <h4>Red Printed T-shirt</h4>
-         <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-o"></i>
-         </div>
-         <p>$50.00</p>
-      </div>
-      <div class="col-4">
-         <img src="{{ asset('assets/images/product-8.jpg') }}" alt="product-8">
-         <h4>Red Printed T-shirt</h4>
-         <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-o"></i>
-         </div>
-         <p>$50.00</p>
-      </div>
-      <div class="col-4">
-         <img src="{{ asset('assets/images/product-9.jpg') }}" alt="product-9">
-         <h4>Red Printed T-shirt</h4>
-         <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-o"></i>
-         </div>
-         <p>$50.00</p>
-      </div>
-      <div class="col-4">
-         <img src="{{ asset('assets/images/product-10.jpg') }}" alt="product-10">
-         <h4>Red Printed T-shirt</h4>
-         <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-half-o"></i>
-         </div>
-         <p>$50.00</p>
-      </div>
-      <div class="col-4">
-         <img src="{{ asset('assets/images/product-11.jpg') }}" alt="product-11">
-         <h4>Red Printed T-shirt</h4>
-         <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-o"></i>
-         </div>
-         <p>$50.00</p>
-      </div>
-      <div class="col-4">
-         <img src="{{ asset('assets/images/product-12.jpg') }}" alt="product-12">
-         <h4>Red Printed T-shirt</h4>
-         <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star-o"></i>
-         </div>
-         <p>$50.00</p>
-      </div>
+      @endforeach
    </div>
    <!-- end all products products -->
-
-   <div class="page-btn">
-       <span>1</span>
-       <span>2</span>
-       <span>3</span>
-       <span>4</span>
-       <span>&#8594;</span>
-   </div>
 </div>
 
 @endsection
