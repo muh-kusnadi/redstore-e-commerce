e5edd4daa4da4b978297ffdeee9f9711045e4191
kusnadi99 <muh.kusnadi99@gmail.com>
2021-03-29 07:40:05 +0700

    add latestProduct data

--- app/Http/Controllers/FrontPageController.php
+++ app/Http/Controllers/FrontPageController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class FrontPageController extends Controller
 {
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
     public function index()
     {
-        return view('pages.front.landing', ['landing' => true, 'headerColor' => true]);
+        $lastestProduct = $this->product->orderBy('rating', 'DESC')->limit(8)->get();
+        // dd($lastestProduct);
+        return view('pages.front.landing', [
+            'landing' => true, 
+            'headerColor' => true,
+            'lastestProduct' => $lastestProduct]);
     }
 }
