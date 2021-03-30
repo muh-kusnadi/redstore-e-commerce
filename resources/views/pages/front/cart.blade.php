09046cb197425be2bc1a2f408e3b55c20aaf2d6f
kusnadi99 <muh.kusnadi99@gmail.com>
2021-03-30 20:26:08 +0700

    conditional show cart list by database

--- resources/views/pages/front/cart.blade.php
+++ resources/views/pages/front/cart.blade.php
@@ -10,54 +10,50 @@
             <th>Subtotal</th>
         </tr>
         <tr>
-            <td>
-                <div class="cart-info">
-                    <img src="{{ asset('assets/images/buy-1.jpg') }}" alt="buy-1">
-                    <div>
-                        <p>Red Printed T-Shirt</p>
-                        <small>Price: $50.00</small>
-                        <br>
-                        <a href="">Remove</a>
+            @forelse ($cart as $item)
+            @php $isCartEmpty = false; @endphp
+            <tr>
+                <td>
+                    <div class="cart-info">
+                        <img src="{{ asset('assets/images/buy-1.jpg') }}" alt="buy-1">
+                        <div>
+                            <p>Red Printed T-Shirt</p>
+                            <small>Price: $50.00</small>
+                            <br>
+                            <a href="">Remove</a>
+                        </div>
                     </div>
-                </div>
-            </td>
-            <td><input type="number" value="1"></td>
-            <td>$50.00</td>
-        </tr>
-        <tr>
-            <td>
-                <div class="cart-info">
-                    <img src="{{ asset('assets/images/buy-2.jpg') }}" alt="buy-2">
-                    <div>
-                        <p>Red Printed T-Shirt</p>
-                        <small>Price: $80.00</small>
-                        <br>
-                        <a href="">Remove</a>
-                    </div>
-                </div>
-            </td>
-            <td><input type="number" value="1"></td>
-            <td>$80.00</td>
-        </tr>
-        <tr>
-            <td>
-                <div class="cart-info">
-                    <img src="{{ asset('assets/images/buy-3.jpg') }}" alt="buy-3">
-                    <div>
-                        <p>Red Printed T-Shirt</p>
-                        <small>Price: $75.00</small>
-                        <br>
-                        <a href="">Remove</a>
-                    </div>
-                </div>
-            </td>
-            <td><input type="number" value="1"></td>
-            <td>$75.00</td>
+                </td>
+                <td><input type="number" value="1"></td>
+                <td>$50.00</td>
+            </tr>
+            @empty
+            @php $isCartEmpty = true; @endphp
+            <tr><td></td></tr>
+            <tr>
+                <td colspan="3" style="text-align: center">Oops! Your cart is empty! Looks like you haven't made your choise yet...</td>
+            </tr>
+            <tr><td></td></tr>
+            @endforelse
         </tr>
     </table>
 
     <div class="total-price">
         <table>
+            @if ($isCartEmpty)
+            <tr>
+                <td>Subtotal</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <td>Tax</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <td>Total</td>
+                <td>-</td>
+            </tr>
+            @else
             <tr>
                 <td>Subtotal</td>
                 <td>$200.00</td>
@@ -70,12 +66,15 @@
                 <td>Total</td>
                 <td>$220.00</td>
             </tr>
+            @endif
         </table>
     </div>
 
+    @if (!$isCartEmpty)
     <div class="checkout">
          <a href="" class="btn">Checkout &#8594;</a>
     </div>
+    @endif
 </div>
 <!-- end cart items details -->
 
