d847307b6848bc776a526fd4a7581d2fb19e2782
kusnadi99 <muh.kusnadi99@gmail.com>
2021-04-12 14:43:31 +0700

    add passport token expired in 120 minutes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Passport\Passport;
+use Carbon\Carbon;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -28,6 +29,7 @@ class AuthServiceProvider extends ServiceProvider
 
         if (! $this->app->routesAreCached()) {
             Passport::routes();
+            Passport::personalAccessTokensExpireIn(Carbon::now()->addMinutes(env('SESSION_LIFETIME')));
         }
     }
 }
